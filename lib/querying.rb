24b38ebc89e110f65dcd1603af57067b264d9151
Srikalyan Megati <kalmegati@gmail.com>
2019-11-12 21:42:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
+  "SELECT series.title, COUNT(species = 'human') FROM series
   LEFT OUTER JOIN characters ON series.author_id = characters.author_id GROUP BY series.title ORDER BY COUNT(species = 'human')"
 end
 

461ca50893a5081719c0609eda678c6fafc4189e
Srikalyan Megati <kalmegati@gmail.com>
2019-11-12 21:35:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM authors
+  INNER JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in

b76437a5a08f45c5cc6f35dfab4d1fbf1c6acb41
Srikalyan Megati <kalmegati@gmail.com>
2019-11-12 21:59:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.id) FROM characters
-  LEFT OUTER JOIN characters_books ON characters.id = characters_books.character_id
-  JOIN books ON characters_books.book_id = books.id"
+  LEFT OUTER JOIN character_books ON characters.id = character_books.character_id
+  JOIN books ON character_books.book_id = books.id"
 end
 
